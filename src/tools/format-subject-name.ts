65c4805fa7b59e2171142d86aff467eab5ea2159
depivew <depiwev@gmail.com>
2026-09-17 13:41:48 +0300

    fix: format subject name

--- src/tools/format-subject-name.ts
+++ src/tools/format-subject-name.ts
@@ -1,6 +1,8 @@
 export function formatSubjectName(sjn: string | null) {
   return sjn
-    ?.replace(/^[А-Яа-яЁё]+\.\d+\s*/, "")
+    ?.replace(/[^А-Яа-яЁёA-Za-z\s]/g, "")
+    .replace(/\s+/g, " ")
     .replace("(РПО)", "")
+    .replace("МДК", "")
     .trim();
 }
